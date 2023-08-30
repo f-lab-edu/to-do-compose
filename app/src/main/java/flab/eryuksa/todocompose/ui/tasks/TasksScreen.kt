9a4f13de4b773c912d13df8d280d19497342ff3e
ERyukSa <jslkap227@gmail.com>
2023-08-30 18:53:28 +0900

    design: add no task

--- app/src/main/java/flab/eryuksa/todocompose/ui/tasks/TasksScreen.kt
+++ app/src/main/java/flab/eryuksa/todocompose/ui/tasks/TasksScreen.kt
@@ -1,5 +1,6 @@
 package flab.eryuksa.todocompose.ui.tasks
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,8 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,13 +24,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import flab.eryuksa.todocompose.R
 import flab.eryuksa.todocompose.ui.theme.ToDoComposeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TasksScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = stringResource(R.string.add_task)
+                )
+            }
+        }
+    ) {
+        NoTask()
+    }
 }
 
 @Composable
@@ -39,23 +62,10 @@ fun TaskComposable(task: Task, onTaskCheckedChange: (Boolean) -> Unit) {
     ) {
         Checkbox(checked = task.isDone, onCheckedChange = onTaskCheckedChange)
         Spacer(Modifier.padding(horizontal = dimensionResource(id = R.dimen.margin_8dp)))
-        Text(text = task.title, textDecoration = if (task.isDone) TextDecoration.LineThrough else TextDecoration.None)
-    }
-}
-
-@Composable
-fun AllTaskList(todoList: List<Task>, doneList: List<Task>) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        when {
-            todoList.isEmpty() && doneList.isEmpty() -> Surface {}
-            todoList.isNotEmpty() && doneList.isEmpty() -> TodoList(todoList)
-            todoList.isEmpty() && doneList.isNotEmpty() -> DoneList(doneList)
-            else -> {
-                TodoList(todoList)
-                Spacer(modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.margin_16dp)))
-                DoneList(doneList)
-            }
-        }
+        Text(
+            text = task.title,
+            textDecoration = if (task.isDone) TextDecoration.LineThrough else TextDecoration.None
+        )
     }
 }
 
@@ -78,16 +88,42 @@ fun TaskList(categoryTitle: String, taskList: List<Task>) {
     }
 }
 
-@Preview
 @Composable
-fun TasksScreenPreview() {
-    ToDoComposeTheme {
-        Surface(
-            color = MaterialTheme.colorScheme.background
-        ) {
-            TasksScreen()
+fun AllTaskList(todoList: List<Task>, doneList: List<Task>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        when {
+            todoList.isEmpty() && doneList.isEmpty() -> Surface {}
+            todoList.isNotEmpty() && doneList.isEmpty() -> TodoList(todoList)
+            todoList.isEmpty() && doneList.isNotEmpty() -> DoneList(doneList)
+            else -> {
+                TodoList(todoList)
+                Spacer(modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.margin_16dp)))
+                DoneList(doneList)
+            }
         }
     }
+}
+
+@Composable
+fun NoTask() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.add_task_please),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Preview(heightDp = 500)
+@Composable
+fun TasksScreenPreview() {
+    TasksScreen()
 }
 
 @Preview
@@ -122,7 +158,7 @@ fun TaskListPreview() {
     }
 }
 
-@Preview
+@Preview(heightDp = 500, showBackground = true)
 @Composable
 fun AllTaskListPreview() {
     val todoList = listOf(Task("할 일", "", false), Task("할 일2", "", false))
@@ -130,10 +166,14 @@ fun AllTaskListPreview() {
     val emptyList: List<Task> = emptyList()
 
     ToDoComposeTheme {
-        Surface(
-            color = MaterialTheme.colorScheme.background
-        ) {
-            AllTaskList(todoList, doneList)
-        }
+        AllTaskList(todoList, doneList)
+    }
+}
+
+@Preview(heightDp = 500, showBackground = true)
+@Composable
+fun NoTaskPreview() {
+    ToDoComposeTheme {
+        NoTask()
     }
 }
