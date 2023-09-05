c12838f8dda71c68064905f8fd12be14d24ad052
ERyukSa <jslkap227@gmail.com>
2023-09-05 16:07:22 +0900

    feat(#6): task 추가 화면 구성
    DONE: 전체적인 틀을 잡았다
    
    TODO: 함수 분리

--- app/src/main/java/flab/eryuksa/todocompose/ui/addtask/AddTaskScreen.kt
+++ app/src/main/java/flab/eryuksa/todocompose/ui/addtask/AddTaskScreen.kt
@@ -1,18 +1,18 @@
 package flab.eryuksa.todocompose.ui.addtask
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,53 +31,81 @@ import flab.eryuksa.todocompose.ui.theme.Padding
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddTaskDialog() {
+fun AddTaskDialog(onClickAdd: () -> Unit, onClickCancel: () -> Unit) {
     var title by remember { mutableStateOf("") }
     var details by remember { mutableStateOf("") }
 
     Dialog(onDismissRequest = { }) {
-        Column {
-            TextField(
-                value = title,
-                onValueChange = { title = it },
-                placeholder = { Text(stringResource(R.string.title)) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = Padding.SMALL),
-                singleLine = true
-            )
-            Spacer(
-                modifier = Modifier.border(
-                    width = 1.dp,
-                    color = Color.DarkGray
+        Surface(
+            modifier = Modifier
+                .height((LocalConfiguration.current.screenHeightDp * 0.5).toInt().dp)
+                .padding(Padding.MEDIUM),
+            shape = RoundedCornerShape(size = Padding.MEDIUM),
+            color = Color.White
+        ) {
+            Column() {
+                TextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    placeholder = { Text(stringResource(R.string.title)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = Padding.SMALL),
+                    singleLine = true,
+                    colors = TextFieldDefaults.textFieldColors(containerColor = Color.White)
                 )
-            )
-            TextField(
-                value = details,
-                onValueChange = { details = it },
-                placeholder = { Text(stringResource(R.string.details)) },
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = Padding.SMALL, end = Padding.SMALL, bottom = Padding.SMALL)
-                    .background(
-                        color = Color.White,
-                        shape = RoundedCornerShape(
-                            topStart = Padding.NONE,
-                            topEnd = Padding.NONE,
-                            bottomStart = Padding.SMALL,
-                            bottomEnd = Padding.SMALL
-                        )
-                    ),
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Start)
-            )
+                TextField(
+                    value = details,
+                    onValueChange = { details = it },
+                    placeholder = { Text(stringResource(R.string.details)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(weight = 1f)
+                        .padding(start = Padding.SMALL, end = Padding.SMALL, bottom = Padding.SMALL),
+                    textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Start),
+                    colors = TextFieldDefaults.textFieldColors(containerColor = Color.White)
+                )
+                Row {
+                    Button(
+                        onClick = onClickCancel,
+                        modifier = Modifier
+                            .weight(weight = 1f)
+                            .padding(horizontal = Padding.MEDIUM)
+                    ) {
+                        Text("취소")
+                    }
+                    Button(
+                        onClick = onClickAdd,
+                        modifier = Modifier
+                            .weight(weight = 1f)
+                            .padding(horizontal = Padding.LARGE)
+                    ) {
+                        Text("추가")
+                    }
+                }
+            }
         }
     }
 }
 
-@Preview(heightDp = 180)
+/*
+@Composable
+fun TitleTextField() {
+    TextField(
+        value = title,
+        onValueChange = { title = it },
+        placeholder = { Text(stringResource(R.string.title)) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Padding.SMALL),
+        singleLine = true
+    )
+}*/
+
+@Preview(heightDp = 360)
 @Composable
 fun AddTaskDialogPreview() {
     Surface {
-        AddTaskDialog()
+        AddTaskDialog({}, {})
     }
 }
